64b131bf3150d7066dfcb1e926dc7e76e64300ca
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2026-04-05 12:51:51 -0500

    Add day 4 in go. All parts

---
+++ go/day04_part1/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type TrashBag struct {
+	x int
+	y int
+}
+
+func parse(filename string) []TrashBag {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic("file error")
+	}
+	lines := strings.Split(strings.Trim(string(data), "\n"), "\n")
+
+	trash_bags := []TrashBag{}
+	for _, line := range lines {
+		split_line := strings.Split(line, ",")
+		x, _ := strconv.Atoi(split_line[0])
+		y, _ := strconv.Atoi(split_line[1])
+		trash_bags = append(trash_bags, TrashBag{x, y})
+	}
+
+	return trash_bags
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func solve(trash_bags []TrashBag) int {
+	current_x, current_y := 0, 0
+	steps := 0
+
+	for _, bag := range trash_bags {
+		steps += abs(current_x-bag.x) + abs(current_y-bag.y)
+		current_x, current_y = bag.x, bag.y
+	}
+	return steps
+}
+
+func solution(filename string) int {
+	trash_bags := parse(filename)
+	return solve(trash_bags)
+}
+
+func main() {
+	fmt.Println(solution("./example.txt")) // 24
+	fmt.Println(solution("./input.txt"))   // 7111
+}
---
+++ go/day04_part3/go.mod
@@ -0,0 +1,3 @@
+module day04_part3
+
+go 1.22.5
