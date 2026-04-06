7beba83338ebbb2cd5b729b3c5c2ea37270d93bf
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2026-04-06 16:16:59 -0500

    Add go solution for day 1 part 2

---
+++ go/day01_part2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func parse(filename string) []string {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic("file error")
+	}
+
+	return strings.Split(strings.Trim(string(data), "\n"), "\n")
+}
+
+func solve(data []string) int {
+	total_sum := 0
+
+	for _, banana := range data {
+		points := strings.Count(banana, "ba")
+		points += strings.Count(banana, "na")
+		points += strings.Count(banana, "ne")
+
+		if points%2 == 0 {
+			total_sum += points
+		}
+	}
+	return total_sum
+}
+
+func solution(filename string) int {
+	data := parse(filename)
+	return solve(data)
+}
+
+func main() {
+	fmt.Println(solution("./example.txt")) // 16
+	fmt.Println(solution("./input.txt"))   // 5250
+}
