93d33b72dff73a123d4c2151b4e193cdc08b738a
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2026-04-07 11:36:15 -0500

    Add go files for day 1 part 3

---
+++ go/day01_part3/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func parse(filename string) []string {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic("file error")
+	}
+
+	return strings.Split(strings.Trim(string(data), "\n"), "\n")
+}
+
+func solve(data []string) int {
+	total_sum := 0
+
+	for _, banana := range data {
+		points := strings.Count(banana, "ne")
+		if points > 0 {
+			continue
+		}
+		points += strings.Count(banana, "ba")
+		points += strings.Count(banana, "na")
+
+		total_sum += points
+	}
+	return total_sum
+}
+
+func solution(filename string) int {
+	data := parse(filename)
+	return solve(data)
+}
+
+func main() {
+	fmt.Println(solution("./example.txt")) // 19
+	fmt.Println(solution("./input.txt"))   // 3161
+}
