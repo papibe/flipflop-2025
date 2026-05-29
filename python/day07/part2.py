0199c34f34bc2501842af50423d89a8f7d138b89
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2026-05-29 15:56:19 -0500

    Add python solution for day 7 part 2

---
+++ python/day07/part2.py
@@ -0,0 +1,44 @@
+from collections import namedtuple
+from math import factorial
+from typing import List
+
+Grid = namedtuple("Grid", ["row", "cols"])
+
+
+def parse(filename: str) -> List[Grid]:
+    with open(filename, "r") as fp:
+        data: List[str] = fp.read().splitlines()
+
+    grids: List[Grid] = []
+    for line in data:
+        values: List[str] = line.split()
+        rows: int = int(values[0])
+        cols: int = int(values[1])
+        grids.append(Grid(rows, cols))
+
+    return grids
+
+
+def solve(grids: List[Grid]) -> int:
+    total_sum: int = 0
+
+    for rows, cols in grids:
+        x: int = rows - 1
+        y: int = cols - 1
+        z: int = x
+        paths: int = factorial(x + y + z) // (
+            factorial(x) * factorial(y) * factorial(z)
+        )
+        total_sum += paths
+
+    return total_sum
+
+
+def solution(filename: str) -> int:
+    data: List[Grid] = parse(filename)
+    return solve(data)
+
+
+if __name__ == "__main__":
+    print(solution("./example.txt"))  # 108
+    print(solution("./input.txt"))  # 4079922
